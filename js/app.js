574e2e5bc361e2401774ac8669521f30f97879b4
Cameron Weston <cameronbweston@gmail.com>
2021-07-01 14:18:58 -0500

    Define constants and starting variables

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const deck = [];
+const playerCash = 1000;
 
-
-
-/*-------------------------------- Variables --------------------------------*/
-
-
+/*--------------------------------Game State Variables --------------------------------*/
+let dealerCards = [];
+let playerCards = [];
+let playerCurrentCash;
+let playerCurrentBet;
 
 /*------------------------ Cached Element References ------------------------*/
 
