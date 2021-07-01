a4d42cddbe2c355ef85e2fecbac6bc9e48517405
Cameron Weston <cameronbweston@gmail.com>
2021-07-01 18:33:36 -0500

    Write more psuedocode and begin stubbing out functions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 const suits = ["spades", "diamonds", "clubs", "hearts"];
 const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const deck = [];
 const playerCash = 1000;
 
 /*--------------------------------Game State Variables --------------------------------*/
@@ -9,7 +8,7 @@ let dealerCards = [];
 let playerCards = [];
 let playerCurrentCash;
 let playerCurrentBet;
-
+let deck = [];
 /*------------------------ Cached Element References ------------------------*/
  const playerCardsEl = document.getElementById('playerCardStack'); //Dont forget to style this so it shows up. This is where the cards will be displayed 
  const dealerCardsEl = document.getElementById('dealerCardStack'); //Dont forget to style this
@@ -19,3 +18,68 @@ let playerCurrentBet;
 
 
 /*-------------------------------- Functions --------------------------------*/
+init();
+
+function init() {
+    //1. This will not be kicked off until user places bet
+    //If user bet, proceed
+
+    //2. refresh all game stats and set everything back to zero
+    //3. call Deal()
+}
+
+function firstDeal() {
+    //Shuffle and create deck
+    deck = []
+    for (i = 0; i < 13; i++) {
+        deck.push(cardValues[i] + 'S');
+        deck.push(cardValues[i] + 'D');
+        deck.push(cardValues[i] + 'C');
+        deck.push(cardValues[i] + 'H');
+    }
+    console.log(deck);
+
+    //Deal a random int index card to the player, remove from deck
+    //Deal a random int index card to the dealer, remove from deck
+
+    //Deal a random int index card to the player, remove from deck
+    //Deal a random int index card card to the dealer, remove from deck
+
+    //Wait for user input...
+}
+
+function playerHit() {
+
+}
+
+function dealerTurn() {
+    //After player is done betting, doubling down, splitting, entering input...
+
+    //Dealer shows all of his cards
+
+    //If dealer has 16 or less, must hit until passes 16
+
+    //call DecideRoundWinner()
+}
+
+function stay () {
+
+}
+
+function doubleDown() {
+    
+}
+
+function split() {
+
+}
+
+function DecideRoundWinner() {
+    //If player wins round...Play celebratory winning audio or animation
+
+    //If player loses round...Play crowd sad audio or animation
+
+    //clear game table and prepare for next round
+
+}
+
