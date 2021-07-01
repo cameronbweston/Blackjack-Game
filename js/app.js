853c904ea8b8368e44c43f8af4e3f4d5177977b3
Cameron Weston <cameronbweston@gmail.com>
2021-07-01 15:06:35 -0500

    Manage to center dealer game mat and align html peices to board

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ let playerCurrentCash;
 let playerCurrentBet;
 
 /*------------------------ Cached Element References ------------------------*/
-
+ 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
