b8def7454cd9efa9b5ce6f0106951aa454edc5af
Cameron Weston <cameronbweston@gmail.com>
2021-07-03 10:43:13 -0500

    Get calculate cards function working

--- js/app.js
+++ js/app.js
@@ -18,27 +18,32 @@ const playButton = document.getElementById('playButton');
 const doubleDownButton = document.getElementById('doubleDownButton');
 const splitButton = document.getElementById('hitButton');
 /*----------------------------- Event Listeners -----------------------------*/
-hitButton.addEventListener('click', hit());
-stayButton.addEventListener('click', stay());
-playButton.addEventListener('click', init());
-doubleDownButton.addEventListener('click', doubleDown());
-splitButton.addEventListener('click', split());
-
+hitButton.addEventListener('click', hit);
+stayButton.addEventListener('click', dealerTurn);
+playButton.addEventListener('click', init);
+doubleDownButton.addEventListener('click', doubleDown);
+splitButton.addEventListener('click', split);
 
 /*-------------------------------- Functions --------------------------------*/
-init();
-firstDeal();
 
 function init() {
+    console.log('init');
     //1. This will not be kicked off until user places bet
     //If user bet, proceed
-
-    //2. refresh all game stats and set everything back to zero
-    //3. call Deal()
+    //placeBet();
+    //2. call Deal()
+    firstDeal();
 }
 
 function reset() {
+    console.log('reset');
+    //2. refresh all game stats and set everything back to zero
+}
 
+function placeBet() {
+    const bet = prompt('Please enter a bet less than 1000');
+    console.log(bet);
+    playerCurrentBet = bet;
 }
 
 function firstDeal() {
@@ -53,13 +58,10 @@ function firstDeal() {
     console.log(deck);
 
     //Deal a random int index card to the player, remove from deck
-    const dealerFirstCard = deck.splice([Math.floor(Math.random() * 52 + 1)], 1);
-    //Deal a random int index card to the dealer, remove from deck
-    const playerFirstCard = deck.splice([Math.floor(Math.random() * 52 + 1)], 1);
-    //Deal a random int index card to the player, remove from deck
-    const dealerSecondCard = deck.splice([Math.floor(Math.random() * 52 + 1)], 1);
-    //Deal a random int index card card to the dealer, remove from deck
-    const playerSecondCard = deck.splice([Math.floor(Math.random() * 52 + 1)], 1);
+    const dealerFirstCard = hit();
+    const playerFirstCard = hit();
+    const dealerSecondCard = hit();
+    const playerSecondCard = hit();
     console.log(deck);
     dealerCards.push(dealerFirstCard, dealerSecondCard);
     playerCards.push(playerFirstCard, playerSecondCard);
@@ -67,25 +69,44 @@ function firstDeal() {
     console.log(`dealer first card: ${dealerFirstCard} dealer second card ${dealerSecondCard} player first card: ${playerFirstCard} player second card:${playerSecondCard}`);
 
     //Render dealer and player card images
+    alert(`Dealer:${dealerCards} Player: ${playerCards}`);
 }
 
-function hit(currentPlayer) {
-    const hit = deck.splice([Math.floor(Math.random() * 52 + 1)], 1);
-    currentPlayer.push(playerHit);
+function hit() {
+    //Return and remove random card from deck
+    return deck.splice([Math.floor(Math.random() * deck.length)], 1);
 }
 
 function dealerTurn() {
     //After player is done betting, doubling down, splitting, entering input...
 
     //Dealer shows all of his cards
-
     //If dealer has 16 or less, must hit until passes 16
+    let handValue = calculateCards(dealerCards);
+    console.log(`handValue: ${handValue}`);
 
+    
     //call DecideRoundWinner()
 }
 
-function stay () {
-
+function calculateCards(playerCardArray) {
+    let total = 0;
+    console.log(`playerCardArray: ${playerCardArray}`);
+    playerCardArray.forEach(card => {
+        console.log(`card: ${card}`)
+        let cardValue = String(card).substring(0,1);
+        console.log(`cardvalue: ${cardValue}`);
+        if (cardValue === 'J' || cardValue === 'Q' || cardValue === 'K') {
+            total += 10;
+        }
+        else if (cardValue === 'A') {
+            total += 11;
+        }
+        else {
+            total += parseInt(cardValue);
+        }
+    })
+    return total;
 }
 
 function doubleDown() {
